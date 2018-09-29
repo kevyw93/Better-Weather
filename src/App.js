a8ec960c7a6bd92bec7c03f48bb1dd7aa952d852
kevwu93 <kevwu93@gmail.com>
2018-09-29 01:09:28 -0400

    added jquery to allow for api to work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {get5DayWeather} from "./weather_api";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {city: ""};
+    this.handleChange = this.handleChange.bind(this);
+    this.grabWeather = this.grabWeather.bind(this);
+  }
+
+  handleChange(field){
+    return (e) => this.setState({[field]: e.target.value});
+  }
+
+  grabWeather(e){
+    e.preventDefault();
+    const cityStr = this.state.city;
+    get5DayWeather(cityStr);
+    debugger
+  }
+
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Hi DUmbo</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
+        <input value={this.state.city} onChange={this.handleChange("city")} placeholder="Input City"/>
+        <button onClick={this.grabWeather}>Submit</button>
+
       </div>
     );
   }
--- src/weather_api.js
+++ src/weather_api.js
@@ -1,8 +1,11 @@
+import $ from "jquery";
+
 export const get5DayWeather = (city) => {
+  debugger
   return $.ajax({
     type: "get",
     url: `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&&APPID=c3c12fbc0ae3a29059ceaa36e4e63189&cnt=5`,
     dataType: "jsonp",
 
-  });
+  }).then(res => console.log(res));
 };
