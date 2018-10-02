716862defc88b0d5563a49381eb11723fc91ec50
kevwu93 <kevwu93@gmail.com>
2018-10-02 16:12:57 -0400

    added a sick loading animation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import DailyComponent from "./daily_component";
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {city: "", weather: null, currCity: null};
+    this.state = {city: "", weather: null, currCity: null, load:false};
     this.handleChange = this.handleChange.bind(this);
     this.grabWeather = this.grabWeather.bind(this);
   }
@@ -16,32 +16,39 @@ class App extends Component {
     return (e) => this.setState({[field]: e.target.value});
   }
 
+
   grabWeather(e){
     e.preventDefault();
+    this.setState({load: true});
     const cityStr = this.state.city;
     let weather;
-    get5DayWeather(cityStr).then(resp => {
+    setTimeout(() => get5DayWeather(cityStr).then(resp => {
       weather = [];
       for(let i = 0; i <resp.list.length; i += 8){
         weather.push(JSON.stringify(resp.list[i]));
       }
       return {weather: weather, currCity: resp.city.name};
-
     }).then((newResp) => {
-      this.setState({weather: newResp.weather, currCity: newResp.currCity, city:""});
-    });
-
+      this.setState({weather: newResp.weather, currCity: newResp.currCity, city:"", load: false});
+    }).catch(() => {
+      this.setState({load: false});
+      alert("Not a valid location.");
+    }), 2000);
   }
 
   render() {
     let DailyWeather;
-    if(this.state.weather){
+    if(this.state.load){
+      DailyWeather = <img src="https://cdn.dribbble.com/users/547544/screenshots/2718569/loading.gif"/>;
+    }else if(this.state.weather){
       const d = new Date();
       const today = d.getDay();
       const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
       DailyWeather = this.state.weather.map( (daily, i) => (
         <DailyComponent id={i} key={i} weather={daily} day={daysOfWeek[(parseInt(today, 10) + i) % 7]}/>
       ));
+    }else{
+      DailyWeather = <main>Hey There! Enter A Location</main>;
     }
     return (
       <main className="App">
